d778393f1747232e3881e8ce77c28eb09ae80511
jeevan <jeevanjames2000@gmail.com>
2024-12-21 17:18:51 +0530

    host

--- body/src/ParentSearch/ParentSearchComp.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { cartState } from "../../host/src/store/slice/hostSlice";
-
-export default function ParentSearchComp() {
-  const searchstate = useSelector((state) => state.module.searchstate);
-  console.log("searchstate: ", searchstate);
-  return <div>ParentSearchComp</div>;
-}
--- head/src/components/profile/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-
-export default function Profile() {
-  return <AccountCircleIcon />;
-}
