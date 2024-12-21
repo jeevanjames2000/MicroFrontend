02f3fd3751abad572261e08ee9f23d58fed6ac8e
jeevan <jeevanjames2000@gmail.com>
2024-12-21 17:37:08 +0530

    public path

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const printCompilationMessage = require("./compilation.config.js");
 
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:5003/",
+    publicPath: "https://microfrontend-f5gd.onrender.com/",
   },
 
   resolve: {
